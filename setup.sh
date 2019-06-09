8b90b28943f21c7499d4c52e407ef064cba11f1c
senz1024 <m5y3s1sunot@gmail.com>
2019-06-09 14:15:51 +0900

    output log

--- setup.sh
+++ setup.sh
@@ -1,10 +1,20 @@
 cp .vimrc ${HOME}
+echo cp .vimrc ${HOME}
 
 if [ ! -e ${HOME}/.vim ]; then
   mkdir ${HOME}/.vim
+  echo mkdir ${HOME}/.vim
 fi
 
 cp .zshrc ${HOME}
+echo cp .zshrc ${HOME}
+
 cp .gitconfig ${HOME}
+echo cp .gitconfig ${HOME}
+
 cp .startup_numpy.py ${HOME}
+echo cp .startup_numpy.py ${HOME}
+
 cp .tmux.conf ${HOME}
+echo .tmux.conf ${HOME}
+
